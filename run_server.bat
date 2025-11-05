abadaac16687078567e1e859fbfc994ff4227579
LyHuu1903 <lytanghuu1903@gmail.com>
2025-11-05 16:08:23 +0700

    thêm client

---
+++ run_server.bat
@@ -0,0 +1,31 @@
+@echo off
+setlocal
+
+pushd "%~dp0"
+
+where java >nul 2>&1
+if errorlevel 1 (
+    echo Java runtime not found in PATH. Install JDK/JRE and try again.
+    pause
+    popd & endlocal & exit /b 1
+)
+
+set CP=
+if exist out set CP=out
+if "%CP%"=="" if exist src set CP=src
+if "%CP%"=="" (
+    echo Cannot locate classpath. Build first with build.bat
+    pause
+    popd & endlocal & exit /b 1
+)
+
+echo Using classpath: %CP%
+java -cp "%CP%" main.TicTacToeServer
+if errorlevel 1 (
+    echo Server exited with error. See messages above.
+    pause
+)
+
+popd
+endlocal
+
